082c09c3d3e29d685ef64b9c78aa07a7b4f2215e
nando <nando@dessena.it>
2017-10-25 12:52:24 +0200

    * Fixed migration timestamps.

--- migrations/2_token_crowdsale_migration.js
+++ migrations/2_token_crowdsale_migration.js
@@ -3,10 +3,10 @@ var DemeterCrowdsale = artifacts.require("DemeterCrowdsale");
 module.exports = function(deployer) {
   /* No need to deploy the token contract, as the crowdsale will create one upon construction */
   deployer.deploy(DemeterCrowdsale,
-    new Date('2017/10/24 12:00:00 GMT+0200').getTime(), // _startTime
-    new Date('2017/10/26 12:00:00 GMT+0200').getTime(), // _endTime
-    new Date('2017/10/24 12:00:00 GMT+0200').getTime(), // _whiteListRegistrationEndTime
-    new Date('2017/10/25 12:00:00 GMT+0200').getTime(), // _whiteListEndTime
+    new Date('2017/10/26 12:00:00 GMT+0200').getTime()/1000, // _startTime
+    new Date('2017/10/28 12:00:00 GMT+0200').getTime()/1000, // _endTime
+    new Date('2017/10/26 12:00:00 GMT+0200').getTime()/1000, // _whiteListRegistrationEndTime
+    new Date('2017/10/27 12:00:00 GMT+0200').getTime()/1000, // _whiteListEndTime
     100, // rate
     web3.toWei(10, 'ether'), // _cap
     web3.toWei(5, 'ether'), // _goal
